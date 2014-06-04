43a161ea9a593b0a3e4157d220bd8b10a56f2dc9
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2014-06-04 14:03:24 -0400

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('firebaseappApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
